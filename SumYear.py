ab6fd419824caef838c75e99a19f5b05ae085639
Jallios <madrive0508@gmail.com>
2023-02-17 12:55:28 +0300

    SumMonth

--- SumYear.py
+++ SumYear.py
@@ -1,83 +1,18 @@
 import calendar
 from tabulate import tabulate
 
-base = {
-   1:{
-        "month":"Январь",
-        "day": 172,
-    },
-    2: {
-        "month":"Февраль",
-        "day": 154,
-        "dayL": 165,
-    },
-    3: {
-        "month":"Март",
-        "day": 172,
-    },
-    4: {
-        "month":"Апрель",
-        "day": 168,
-    },
-    5: {
-        "month":"Май",
-        "day": 172,
-    },
-    6: {
-        "month":"Июнь",
-        "day": 168,
-    },
-    7: {
-        "month":"Июль",
-        "day": 172,
-    },
-    8: {
-        "month":"Август",
-        "day": 172,
-    },
-    9: {
-        "month":"Сентябрь",
-        "day": 168,
-    },
-    10: {
-        "month":"Октябрь",
-        "day": 172,
-    },
-    11: {
-        "month":"Ноябрь",
-        "day": 168,
-    },
-    12: {
-        "month":"Декабрь",
-        "day": 172,
-    }
-}
-
 year = int(input('Введите год: '))
 
-result = 0
+if calendar.isleap(year): 
+    februrary = 29
+else: 
+    februrary = 28
 
-if calendar.isleap(year):
-    for key in base:
-        if key == 2:
-            m = (base[key].get("month"))
-            d = (base[key].get("dayL"))
-            result += d
-        else:
-            m = (base[key].get("month"))
-            d = (base[key].get("day"))
-            result += d
-        print(f"\n{tabulate([[m,d]], headers=['Месяц', 'Дней'])}")
-    print(f"\nГод високосный.\nРезультат: {result}")
-   
-else:
-    for key in base:
-        m = (base[key].get("month"))
-        d = (base[key].get("day"))
-        result += d
-        print(f"\n{tabulate([[m,d]], headers=['Месяц', 'Дней'])}")
-    print(f"\nГод не високосный.\nРезультат: {result}")
+rezult = 0
+array = [31, februrary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-       
-
-
+for i in range(12):
+    for c in range(array[i] + 1):
+        print(array[i])
+        rezult += c//10 + c%10
+print(rezult)
